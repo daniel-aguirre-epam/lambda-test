459e8ec2eb8f4f9028c45c94699b660dadb08bfd
Daniel Aguirre Buitrago <110441548+daniel-aguirre-epam@users.noreply.github.com>
2024-09-13 08:12:21 -0500

    Update DynamoDbItem.java

--- task05/jsrc/main/java/com/task05/DynamoDbItem.java
+++ task05/jsrc/main/java/com/task05/DynamoDbItem.java
@@ -24,7 +24,7 @@ public class DynamoDbItem {
         this.body = body;
     }
 
-    @DynamoDbPartitionKey(name = "id")
+    @DynamoDbPartitionKey
     public String getId() {
         return id;
     }
