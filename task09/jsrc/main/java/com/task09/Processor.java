8f47004675e1bcf447bf21f3bc060bdc75446095
Daniel.Aguirre.ND <daniel.aguirre.-nd@disney.com>
2024-10-04 18:17:36 -0500

    lambda + xray

--- task09/jsrc/main/java/com/task09/Processor.java
+++ task09/jsrc/main/java/com/task09/Processor.java
@@ -10,9 +10,11 @@ import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.Architecture;
 import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.TracingMode;
 import com.syndicate.deployment.model.lambda.url.AuthType;
@@ -49,6 +51,10 @@ import java.util.UUID;
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
+@DependsOn(
+		name = "Weather",
+		resourceType = ResourceType.DYNAMODB_TABLE
+)
 public class Processor implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
 	Gson gson = new Gson();
@@ -59,7 +65,7 @@ public class Processor implements RequestHandler<APIGatewayV2HTTPEvent, APIGatew
 
 	public Processor() {
 		dynamoDbClient = DynamoDbEnhancedClient.create();
-		table = dynamoDbClient.table(System.getenv("table"), TableSchema.fromBean(WeatherForecast.class));
+		table = dynamoDbClient.table("cmtr-326d7d29-Weather-test", TableSchema.fromBean(WeatherForecast.class));
 	}
 
 	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
